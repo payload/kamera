85702b3fd25dc9df75fbd164940a721d203a886d
Gilbert Röhrbein <payload.git@mailbox.org>
2023-07-23 15:32:42 +0200

    basic unit tests work for v4l2

--- src/linux_v4l2/mod.rs
+++ src/linux_v4l2/mod.rs
@@ -1,52 +1,130 @@
+use v4l;
+use v4l::io::traits::CaptureStream;
+use v4l::prelude::*;
+use v4l::video::capture::Parameters;
+use v4l::video::Capture;
+use v4l::*;
+
+use std::{
+    sync::{mpsc::*, RwLock},
+    thread,
+};
+
 use crate::{InnerCamera, InnerFrame, InnerFrameData};
 
-#[derive(Debug)]
-pub struct Camera {}
+pub struct Camera {
+    device: RwLock<v4l::Device>,
+    device_name: String,
+    stream: RwLock<Option<v4l::io::mmap::Stream<'static>>>,
+}
 
 impl InnerCamera for Camera {
     type Frame = Frame;
 
     fn new_default_device() -> Self {
-        todo!()
+        let device_node = v4l::context::enum_devices().into_iter().next().unwrap();
+        let device_name =
+            device_node.name().unwrap_or_else(|| device_node.path().to_string_lossy().to_string());
+
+        println!(
+            "Node {{ index: {}, name: {:?}, path: {:?} }}",
+            device_node.index(),
+            device_node.name(),
+            device_node.path()
+        );
+
+        let mut device = v4l::Device::new(0).unwrap();
+
+        for fmt in device.enum_formats().unwrap() {
+            println!("{:?}", fmt);
+
+            for size in device.enum_framesizes(fmt.fourcc).unwrap() {
+                println!("{:?}", size);
+            }
+        }
+
+        let rgb = FourCC::new(b"RGB3");
+        let mut fmt = device.format().unwrap();
+        let size = device
+            .enum_framesizes(fmt.fourcc)
+            .unwrap()
+            .pop()
+            .unwrap()
+            .size
+            .to_discrete()
+            .into_iter()
+            .last()
+            .unwrap();
+        fmt.width = size.width;
+        fmt.height = size.height;
+        let fmt = device.set_format(&fmt).expect("Failed to write format");
+
+        Self { device: RwLock::new(device), device_name, stream: RwLock::new(None) }
     }
 
     fn start(&self) {
-        todo!()
+        let device = self.device.write().unwrap();
+        let stream =
+            v4l::io::mmap::Stream::with_buffers(&device, v4l::buffer::Type::VideoCapture, 4)
+                .expect("Failed to create buffer stream");
+        let _ = self.stream.write().unwrap().insert(stream);
     }
 
     fn stop(&self) {
-        todo!()
+        let _ = self.stream.write().unwrap().take();
     }
 
     fn wait_for_frame(&self) -> Option<Frame> {
-        todo!()
+        let format = self.device.read().unwrap().format().unwrap();
+        if let Ok((buf, meta)) = self.stream.write().unwrap().as_mut().unwrap().next() {
+            let data = match &format.fourcc.repr {
+                b"RGB3" => buf.to_vec(),
+                b"YUYV" => vec![],
+                b"MJPG" => todo!("NJPG not implemented"),
+                _ => panic!("invalid buffer pixelformat"),
+            };
+
+            Some(Frame { data })
+        } else {
+            None
+        }
+    }
+}
+
+impl std::fmt::Debug for Camera {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Camera").field("device", &self.device_name).finish()
     }
 }
 
 #[derive(Debug)]
-pub struct Frame {}
+pub struct Frame {
+    data: Vec<u8>,
+}
 
 impl InnerFrame for Frame {
     type FrameData = FrameData;
 
     fn data(&self) -> FrameData {
-        todo!()
+        FrameData { data: self.data.clone() }
     }
 
     fn size_u32(&self) -> (u32, u32) {
-        todo!()
+        (1280, 1024)
     }
 }
 
 #[derive(Debug)]
-pub struct FrameData {}
+pub struct FrameData {
+    data: Vec<u8>,
+}
 
 impl InnerFrameData for FrameData {
     fn data_u8(&self) -> &[u8] {
-        todo!()
+        &self.data
     }
 
     fn data_u32(&self) -> &[u32] {
-        todo!()
+        unsafe { self.data.align_to().1 }
     }
 }
