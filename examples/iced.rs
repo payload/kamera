3b330f1f49be17c29956d462bd9b46a2c2947e80
Gilbert Röhrbein <payload.git@mailbox.org>
2023-08-13 16:18:50 +0200

    iced: initial camera example integration

--- examples/iced.rs
+++ examples/iced.rs
@@ -1,81 +1,13 @@
-//! This example showcases a drawing a quad.
-mod quad {
-    use iced::advanced::layout::{self, Layout};
-    use iced::advanced::renderer;
-    use iced::advanced::widget::{self, Widget};
-    use iced::mouse;
-    use iced::{Color, Element, Length, Rectangle, Size};
+use std::iter::repeat;
+use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
+use std::time::Instant;
 
-    pub struct CustomQuad {
-        size: f32,
-        radius: [f32; 4],
-        border_width: f32,
-    }
-
-    impl CustomQuad {
-        pub fn new(size: f32, radius: [f32; 4], border_width: f32) -> Self {
-            Self {
-                size,
-                radius,
-                border_width,
-            }
-        }
-    }
-
-    impl<Message, Renderer> Widget<Message, Renderer> for CustomQuad
-    where
-        Renderer: renderer::Renderer,
-    {
-        fn width(&self) -> Length {
-            Length::Shrink
-        }
-
-        fn height(&self) -> Length {
-            Length::Shrink
-        }
-
-        fn layout(
-            &self,
-            _renderer: &Renderer,
-            _limits: &layout::Limits,
-        ) -> layout::Node {
-            layout::Node::new(Size::new(self.size, self.size))
-        }
-
-        fn draw(
-            &self,
-            _state: &widget::Tree,
-            renderer: &mut Renderer,
-            _theme: &Renderer::Theme,
-            _style: &renderer::Style,
-            layout: Layout<'_>,
-            _cursor: mouse::Cursor,
-            _viewport: &Rectangle,
-        ) {
-            renderer.fill_quad(
-                renderer::Quad {
-                    bounds: layout.bounds(),
-                    border_radius: self.radius.into(),
-                    border_width: self.border_width,
-                    border_color: Color::from_rgb(1.0, 0.0, 0.0),
-                },
-                Color::BLACK,
-            );
-        }
-    }
-
-    impl<'a, Message, Renderer> From<CustomQuad> for Element<'a, Message, Renderer>
-    where
-        Renderer: renderer::Renderer,
-    {
-        fn from(circle: CustomQuad) -> Self {
-            Self::new(circle)
-        }
-    }
-}
-
-use iced::widget::{column, container, slider, text};
-use iced::{Alignment, Element, Length, Sandbox, Settings};
+use iced::widget::{column, container, image, slider, text};
+use iced::{
+    executor, window, Alignment, Application, Command, Element, Length, Settings, Subscription,
+    Theme,
+};
+use kamera::Camera;
 
 pub fn main() -> iced::Result {
     Example::run(Settings::default())
@@ -84,6 +16,10 @@ pub fn main() -> iced::Result {
 struct Example {
     radius: [f32; 4],
     border_width: f32,
+    #[allow(unused)]
+    end_camera: Sender<()>,
+    camera_frame: Receiver<((u32, u32), Vec<u8>)>,
+    current_frame: image::Handle,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -94,25 +30,71 @@ enum Message {
     RadiusBottomRightChanged(f32),
     RadiusBottomLeftChanged(f32),
     BorderWidthChanged(f32),
+    Tick(Instant),
 }
 
-impl Sandbox for Example {
+impl Application for Example {
     type Message = Message;
+    type Executor = executor::Default;
+    type Theme = Theme;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            radius: [50.0; 4],
-            border_width: 0.0,
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        let (end_camera, end) = channel::<()>();
+        let (send_frame, camera_frame) = channel::<((u32, u32), Vec<u8>)>();
+        std::thread::spawn(move || {
+            let camera = Camera::new_default_device();
+            camera.start();
+            let mut keep_going = true;
+            while keep_going {
+                if Err(TryRecvError::Empty) != end.try_recv() {
+                    keep_going = false;
+                }
+                if let Some(frame) = camera.wait_for_frame() {
+                    let size = frame.size_u32();
+                    let pixels = frame.data().data_u8().to_vec();
+                    if send_frame.send((size, pixels)).is_err() {
+                        keep_going = false;
+                    }
+                }
+            }
+            camera.stop();
+        });
+
+        (
+            Self {
+                radius: [50.0; 4],
+                border_width: 0.0,
+                end_camera,
+                camera_frame,
+                current_frame: image::Handle::from_pixels(
+                    16,
+                    16,
+                    Vec::from_iter(repeat([0, 0, 0, 0]).take(16 * 16).flatten()),
+                ),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("Custom widget - Iced")
     }
 
-    fn update(&mut self, message: Message) {
+    fn subscription(&self) -> Subscription<Message> {
+        window::frames().map(Message::Tick)
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
         let [tl, tr, br, bl] = self.radius;
+
         match message {
+            Message::Tick(instant) => {
+                while let Ok(((w, h), pixels)) = self.camera_frame.try_recv() {
+                    self.current_frame = image::Handle::from_pixels(w, h, pixels);
+                    println!("frame. {}ms per frame", instant.elapsed().as_millis());
+                }
+            }
             Message::RadiusTopLeftChanged(radius) => {
                 self.radius = [radius, tr, br, bl];
             }
@@ -129,33 +111,27 @@ impl Sandbox for Example {
                 self.border_width = width;
             }
         }
+
+        Command::none()
     }
 
     fn view(&self) -> Element<Message> {
         let [tl, tr, br, bl] = self.radius;
 
         let content = column![
-            quad::CustomQuad::new(200.0, self.radius, self.border_width),
+            image(self.current_frame.clone()),
             text(format!("Radius: {tl:.2}/{tr:.2}/{br:.2}/{bl:.2}")),
             slider(1.0..=100.0, tl, Message::RadiusTopLeftChanged).step(0.01),
             slider(1.0..=100.0, tr, Message::RadiusTopRightChanged).step(0.01),
-            slider(1.0..=100.0, br, Message::RadiusBottomRightChanged)
-                .step(0.01),
-            slider(1.0..=100.0, bl, Message::RadiusBottomLeftChanged)
-                .step(0.01),
-            slider(1.0..=10.0, self.border_width, Message::BorderWidthChanged)
-                .step(0.01),
+            slider(1.0..=100.0, br, Message::RadiusBottomRightChanged).step(0.01),
+            slider(1.0..=100.0, bl, Message::RadiusBottomLeftChanged).step(0.01),
+            slider(1.0..=10.0, self.border_width, Message::BorderWidthChanged).step(0.01),
         ]
         .padding(20)
         .spacing(20)
         .max_width(500)
         .align_items(Alignment::Center);
 
-        container(content)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_x()
-            .center_y()
-            .into()
+        container(content).width(Length::Fill).height(Length::Fill).center_x().center_y().into()
     }
 }
